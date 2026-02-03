ae631914dff69f3bacf45300238613b30493384a
PerinPatel03 <perinpatel2233@gmail.com>
2026-02-03 13:36:29 +0530

    removed dotenv main function code

--- src/main/java/com/myproject/rentalshopms/RentalshopmsApplication.java
+++ src/main/java/com/myproject/rentalshopms/RentalshopmsApplication.java
@@ -9,11 +9,11 @@ public class RentalshopmsApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure().load();
-
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		Dotenv dotenv = Dotenv.configure().load();
+//
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(RentalshopmsApplication.class, args);
 	}
